07fd90dd6167a5836074cc6d72e67c134eacd34a
TheBinaryGuy <hello@thebinary.dev>
2024-03-06 20:05:29 +0530

    feat: added alert component, used alert in the waitlist to note that we do not spam users

--- src/app/(marketing)/waitlist.tsx
+++ src/app/(marketing)/waitlist.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useState } from 'react';
-import { Loader2 } from 'lucide-react';
+import { Loader2, Terminal } from 'lucide-react';
 
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { api } from '@/trpc/react';
@@ -20,32 +21,41 @@ export function Waitlist() {
     }
 
     return (
-        <form
-            onSubmit={e => {
-                e.preventDefault();
-                mutate({
-                    email,
-                });
-            }}
-            className='flex max-w-prose items-center gap-2'>
-            <Input
-                name='email'
-                id='email'
-                type='email'
-                placeholder='hey@hushos.com'
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                required
-            />
-            <Button disabled={isPending}>
-                {isPending ? (
-                    <>
-                        <Loader2 className='mr-2 size-4 animate-spin' /> Joining
-                    </>
-                ) : (
-                    'Join'
-                )}
-            </Button>
-        </form>
+        <div className='max-w-prose space-y-4'>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    mutate({
+                        email,
+                    });
+                }}
+                className='flex items-center gap-2'>
+                <Input
+                    name='email'
+                    id='email'
+                    type='email'
+                    placeholder='hey@hushos.com'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <Button disabled={isPending}>
+                    {isPending ? (
+                        <>
+                            <Loader2 className='mr-2 size-4 animate-spin' /> Joining
+                        </>
+                    ) : (
+                        'Join'
+                    )}
+                </Button>
+            </form>
+            <Alert variant='info'>
+                <Terminal className='size-4' />
+                <AlertTitle>Note</AlertTitle>
+                <AlertDescription>
+                    We only use your email to notify you when we launch. We will not spam you.
+                </AlertDescription>
+            </Alert>
+        </div>
     );
 }
