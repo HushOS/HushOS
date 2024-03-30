f6ca04da75f7ee071fe80e84aebac34f8cdb65e1
TheBinaryGuy <hello@thebinary.dev>
2024-03-30 21:06:43 +0530

    build: set some default envs and build config change

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,21 @@ import createJiti from 'jiti';
 const jiti = createJiti(new URL(import.meta.url).pathname);
 
 const {
-    serverEnvs: { ANALYZE },
+    serverEnvs: { ANALYZE, DEPLOY_TARGET },
 } = jiti('./src/env/server');
-jiti('./src/env/client');
+const {
+    clientEnvs: { NEXT_PUBLIC_DOMAIN },
+} = jiti('./src/env/client');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: DEPLOY_TARGET === 'standalone' ? 'standalone' : undefined,
     swcMinify: true,
     reactStrictMode: true,
     images: {
         remotePatterns: [
             {
-                hostname: process.env.NEXT_PUBLIC_DOMAIN ?? 'hushos.com',
+                hostname: NEXT_PUBLIC_DOMAIN ?? 'hushos.com',
             },
         ],
         formats: ['image/avif', 'image/webp'],
